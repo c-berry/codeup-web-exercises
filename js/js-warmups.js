8918f90bf6c7c20ddbd10b533eae3d89d3fd112a
Christopher Berry <christopher.m.berry01@gmail.com>
2022-02-07 12:07:09 -0600

    create and work on functions warmups and loops lecture

---
+++ js/js-warmups.js
@@ -0,0 +1,16 @@
+"use strict";
+//write function takes input return the string "I
+//have no idea what you just said."
+
+function noIdea(input) {
+    return "I have no idea what you just said.";
+}
+console.log(noIdea(5));
+console.log(noIdea("hello"));
+console.log(noIdea());
+
+function timesTwo(number) {
+    return number * 2;
+}
+console.log(timesTwo(2));
+console.log(timesTwo(100));
