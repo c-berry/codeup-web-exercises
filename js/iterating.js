039902274e30bacc478aae36a11092ab1965c1da
Christopher Berry <christopher.m.berry01@gmail.com>
2022-02-08 16:00:12 -0600

    create iterating arrays files

---
+++ js/iterating.js
@@ -0,0 +1,99 @@
+(function(){
+    "use strict";
+
+    /**
+     * TODO:
+     * Create an array of 4 people's names and store it in a variable called
+     * 'names'.
+     */
+
+    var names = ["Bob", "Sally", "Jimmy", "Johnny"];
+
+    /**
+     * TODO:
+     * Create a log statement that will log the number of elements in the names
+     * array.
+     */
+
+    // console.log(names.length);
+
+    /**
+     * TODO:
+     * Create log statements that will print each of the names individually by
+     * accessing each element's index.
+     */
+
+    // console.log(names[0]);
+    // console.log(names[1]);
+    // console.log(names[2]);
+    // console.log(names[3]);
+
+    /**
+     * TODO:
+     * Write some code that uses a for loop to log every item in the names
+     * array.
+     */
+
+    // for (var i = 0; i < names.length; i++) {
+    //     console.log(names[i]);
+    // }
+
+    /**
+     * TODO:
+     * Refactor your above code to use a `forEach` loop
+     */
+
+    // names.forEach(function(name) {
+    //     console.log(name)
+    // });
+
+    /**
+     * TODO:
+     * Create the following three functions, each will accept an array and
+     * return an an element from it
+     * - first: returns the first item in the array
+     * - second: returns the second item in the array
+     * - last: returns the last item in the array
+     *
+     * Example:
+     *  > first([1, 2, 3, 4, 5]) // returns 1
+     *  > second([1, 2, 3, 4, 5]) // returns 2
+     *  > last([1, 2, 3, 4, 5]) // return 5
+     */
+    var numbers = [1,2,3,4,5];
+    var numberz = [6, 7, 8, 9, 10];
+    // console.log(numbers[0]);
+
+    // function number1() {
+    //     console.log(numbers[0]);
+    // }
+    // number1();
+    //
+    // function number2() {
+    //     console.log(numbers[1]);
+    // }
+    // number2();
+    //
+    // function number5() {
+    //     console.log(numbers[4]);
+    // }
+    // number5();
+
+    function number1(array) {
+        console.log(array[0]);
+    }
+    number1(numbers);
+
+    function number2(array) {
+        console.log(array[1]);
+    }
+    number2(numbers);
+
+    function number5(array) {
+        console.log(array[4]);
+    }
+    number5(numbers);
+
+    number5(numberz);
+
+})();
