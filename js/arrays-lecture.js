69d6d37e0bafb81d89b32d7f7bf14837bcdd115e
Christopher Berry <christopher.m.berry01@gmail.com>
2022-02-09 10:19:33 -0600

    create planets array html and js files and complete exercises

--- js/arrays-lecture.js
+++ js/arrays-lecture.js
@@ -1,9 +1,9 @@
-// var groceryList = ["bread", "eggs", "butter", "milk"];
+var groceryList = ["bread", "eggs", "butter", "milk"];
 // console.log(groceryList[2]);
 // console.log("I have to buy " + groceryList[0]);
 
 //create array of 4 family, friends, or pets:
-// var family = ["mom", "dad", "grandma", "grandpa"];
+var family = ["mom", "dad", "grandma", "grandpa"];
 
 //console.log the first and last item
 // console.log(family[0], family[3]);
@@ -25,7 +25,7 @@
 // }
 
 // Given an array
-// var cities = ["San Antonio", "Austin", "Dallas", "Houston"];
+var cities = ["San Antonio", "Austin", "Dallas", "Houston"];
 // First, create a for loop that generates the output:
 //     ```
 // San Antonio
@@ -49,6 +49,42 @@
 var prices = [32.99, 21.99, 6.99, 4.99, 12.99, 8.98, 5.99];
 
 //Second parameter always represents index
-prices.forEach(function(price, index) {
-    console.log("Item number " + (index + 1) + " costs $" + price);
-});
+// prices.forEach(function(price, index) {
+//     console.log("Item number " + (index + 1) + " costs $" + price);
+
+    // groceryList.push("potatoes");
+    // console.log(groceryList);
+    // groceryList.pop();
+    // console.log(groceryList);
+    //
+    // groceryList.unshift("sliced cheddar");
+    // console.log(groceryList);
+    // groceryList.shift();
+    // console.log(groceryList);
+    //
+    // var indexOfButter = groceryList.indexOf("butter");
+    // // console.log(groceryList.indexOf("butter"));
+    // console.log("The buuter is at the index " + indexOfButter);
+    //
+    // groceryList[2] = "peanut butter";
+    // console.log(groceryList);
+    // groceryList[indexOfButter] = "butter";
+    // console.log(groceryList);
+
+// var firstHalfOfList = groceryList.slice(0, 2);
+// console.log(firstHalfOfList);
+// console.log(groceryList);
+// var secondHalfOfList = groceryList.slice(2);
+// console.log(secondHalfOfList);
+// firstHalfOfList.push("peanut butter");
+// console.log(firstHalfOfList);
+// groceryList = firstHalfOfList.concat(secondHalfOfList);
+// console.log(groceryList)
+// console.log(firstHalfOfList)
+// console.log(secondHalfOfList);
+
+groceryList.reverse();
+console.log(groceryList);
+groceryList.sort();
+console.log(groceryList);
+
