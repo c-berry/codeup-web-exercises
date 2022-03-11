64c285f117b9480c8a50963d271a87f1d770640a
Christopher Berry <christopher.m.berry01@gmail.com>
2022-03-11 09:29:26 -0600

    complete warmup js exercise

--- js/js-warmups.js
+++ js/js-warmups.js
@@ -522,29 +522,47 @@
 
 // Examples:
 
-var nums1 = [1, 2, 3, 4, 5];
-var nums2 = [2, 2, 3, 4, 5];
-var nums3 = [10, 0, -5, 4, 5];
-var nums4 = [2, 2, -5, 2, 2];
-var nums5 = [2, 2, 2, 2, 2];
+// var nums1 = [1, 2, 3, 4, 5];
+// var nums2 = [2, 2, 3, 4, 5];
+// var nums3 = [10, 0, -5, 4, 5];
+// var nums4 = [2, 2, -5, 2, 2];
+// var nums5 = [2, 2, 2, 2, 2];
 
 //  WORKS:
-function addTwos(arr, num) {
-    var total = 0;
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] === num) {
-            total += arr[i];
-        }
-    }
-    return total;
-}
-console.log(addTwos(nums1, 2));
-console.log(addTwos(nums2, 2));
-console.log(addTwos(nums3,2));
-console.log(addTwos(nums4,2));
-console.log(addTwos(nums5,2));
+// function addTwos(arr, num) {
+//     var total = 0;
+//     for (var i = 0; i < arr.length; i++) {
+//         if (arr[i] === num) {
+//             total += arr[i];
+//         }
+//     }
+//     return total;
+// }
+// console.log(addTwos(nums1, 2));
+// console.log(addTwos(nums2, 2));
+// console.log(addTwos(nums3,2));
+// console.log(addTwos(nums4,2));
+// console.log(addTwos(nums5,2));
 // addTwos(nums1) // returns 2
 // addTwos(nums2) // returns 4
 // addTwos(nums3) // returns 0
 // addTwos(nums4) // returns 8
 // addTwos(nums5) // returns 10
+
+// Write a function, returnStringObject, that takes in a string and returns an object with information about the string. The object should have properties for stringValue and stringArray.
+//BOTH WORK THE SAME:
+function returnStringObject(str) {
+    var strValue = {};
+    strValue.stringValue = str;
+    strValue.stringArray = str.split("");
+    return strValue;
+}
+console.log(returnStringObject("hello"));
+
+function returnStringObject2(str) {
+    return {
+        stringValue: str,
+        stringArray: str.split("")
+    }
+}
+console.log(returnStringObject2("hello"));
