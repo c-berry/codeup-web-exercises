8dc6302550d09314169576b0fd77d4d87126a3ae
Christopher Berry <christopher.m.berry01@gmail.com>
2022-03-15 15:27:27 -0500

    complete weather project functionality. modify geocode utils

--- js/mapbox-geocoder-utils.js
+++ js/mapbox-geocoder-utils.js
@@ -51,3 +51,17 @@ function reverseGeocode(coordinates, token) {
             return data.features[0].place_name;
         });
 }
+
+//TARGETS CITY, STATE, COUNTY ONLY>
+function reverseGeocode2(coordinates, token) {
+    var baseUrl = 'https://api.mapbox.com';
+    var endPoint = '/geocoding/v5/mapbox.places/';
+    return fetch(baseUrl + endPoint + coordinates.lng + "," + coordinates.lat + '.json' + "?" + 'access_token=' + token)
+        .then(function(res) {
+            return res.json();
+        })
+        // to get all the data from the request, comment out the following three lines...
+        .then(function(data) {
+            return data.features[3].place_name;
+        });
+}
